084979cddf943389829895cca137a104f6798732
Mykhailo Pyroh <mykhailopk@gmail.com>
2026-03-15 20:28:52 +0100

    ready

---
+++ src/middleware/authenticate.js
@@ -0,0 +1,38 @@
+import createHttpError from 'http-errors';
+import Session from '../models/session.js';
+import User from '../models/user.js';
+
+export const authenticate = async (req, res, next) => {
+    const { accessToken } = req.cookies;
+
+    // 1. Перевірка наявності accessToken
+    if (!accessToken) {
+      throw createHttpError(401, 'Missing access token');
+    }
+
+    // 2. Пошук сесії
+    const session = await Session.findOne({ accessToken });
+
+    if (!session) {
+      throw createHttpError(401, 'Session not found');
+    }
+
+    // 3. Перевірка терміну дії accessToken
+    const isExpired = new Date() > new Date(session.accessTokenValidUntil);
+
+    if (isExpired) {
+      throw createHttpError(401, 'Access token expired');
+    }
+
+    // 4. Пошук користувача
+    const user = await User.findById(session.userId);
+
+    if (!user) {
+      throw createHttpError(401);
+    }
+
+    // 5. Додаємо користувача у request
+    req.user = user;
+
+    next();
+};
---
+++ src/models/user.js
@@ -0,0 +1,43 @@
+import mongoose from "mongoose";
+
+const { Schema, model } = mongoose;
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 8,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+userSchema.methods.toJSON = function () {
+  const obj = this.toObject();
+  delete obj.password;
+  return obj;
+};
+
+userSchema.pre("save", function (next) {
+  if (this.isNew && !this.username) {
+    this.username = this.email;
+  }
+  next();
+});
+
+const User = model("User", userSchema);
+
+export default User;
